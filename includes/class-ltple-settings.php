7bdd359f5d1a0665ab65796a770a8955008298f0
Rafasashi\Raphael <raphael.dartigues@gmail.com>
2017-05-04 14:59:12 +0200

    plugin info added

--- includes/class-ltple-settings.php
+++ includes/class-ltple-settings.php
@@ -43,9 +43,25 @@ class LTPLE_Affiliate_Settings {
 		$this->plugin 		 	= new stdClass();
 		$this->plugin->slug  	= 'live-template-editor-affiliate';
 		
-		add_action('ltple_addon_settings', array($this, 'settings_fields' ) );
+		add_action('ltple_plugin_settings', array($this, 'plugin_info' ) );
+		
+		add_action('ltple_plugin_settings', array($this, 'settings_fields' ) );
 		
 		add_action( 'ltple_admin_menu' , array( $this, 'add_menu_items' ) );	
+	}
+	
+	public function plugin_info(){
+		
+		$this->parent->settings->addons['affiliate-program'] = array(
+			
+			'title' 		=> 'Affiliate Program',
+			'addon_link' 	=> 'https://github.com/rafasashi/live-template-editor-affiliate',
+			'addon_name' 	=> 'live-template-editor-affiliate',
+			'source_url' 	=> 'https://github.com/rafasashi/live-template-editor-affiliate/archive/master.zip',
+			'description'	=> 'Affiliate program inluding click tracking and commissions...',
+			'author' 		=> 'Rafasashi',
+			'author_link' 	=> 'https://profiles.wordpress.org/rafasashi/',
+		);		
 	}
 
 	/**
